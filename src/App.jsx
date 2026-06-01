1be38189d2d01434afdd83b011fa003a129ef9fc
ChathuraSamarakoon <chathurasamarakoon849495@gmail.com>
2026-06-02 01:52:11 +0530

    chore: clean start of react frontend with vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-4xl font-extrabold text-center text-red-600 mt-8 mb-6 shadow-sm">
+       🍔 Fudo Restaurant Menu
+    </h1>
+  );
+}
+
+export default App;
