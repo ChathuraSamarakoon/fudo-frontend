4f6bd97ea0fd9b0a3e4029363a5c56d3de45233c
ChathuraSamarakoon <chathurasamarakoon849495@gmail.com>
2026-06-05 01:46:28 +0530

    Adding AWS backend api link

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api', 
+    
+    baseURL: 'http://fudo-backend-env.eba-6pzh9mzc.ap-southeast-1.elasticbeanstalk.com/api', 
 });
 
 export default api;
